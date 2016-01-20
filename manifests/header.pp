0fce91a1af94ae78e80617a719f78f4279c1fa06
Adam Breaux <breauxa@email.chop.edu>
2016-01-20 10:47:36 -0500

    refit for this module

--- manifests/header.pp
+++ manifests/header.pp
@@ -1,13 +1,16 @@
-define apache::header (
+define httpd::header (
   $headers = '',
   $requestheaders = ''
 ) {
+  include ::httpd
+
   file { '/etc/httpd/conf.d/header.conf':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template('apache/header.erb'),
+    notify  => Service[$::httpd::params::httpd_service],
   }
 
 }
