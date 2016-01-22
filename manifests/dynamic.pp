0ef3fde3ab1569b47f064c2dfb7c0465a03f84c9
Adam Breaux <breauxa@email.chop.edu>
2016-01-22 12:27:55 -0500

    this should make no difference!

--- manifests/dynamic.pp
+++ manifests/dynamic.pp
@@ -11,6 +11,8 @@ define httpd::dynamic (
 ) {
   include ::httpd
 
+  $httpd_version = $::httpd_version
+
   file { "/etc/httpd/vhost.d/${priority}-${name}.conf":
     ensure  => present,
     owner   => 'root',
